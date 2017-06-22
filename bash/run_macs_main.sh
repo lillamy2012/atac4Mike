afd2ee9ebbeb58848b84a84f302e7d2e7c25a057
Axelsson <elin.axelsson@login1.ice.gmi.oeaw.ac.at>
2017-06-22 11:50:23 +0000

    fixed bug

--- bash/run_macs_main.sh
+++ bash/run_macs_main.sh
@@ -37,7 +37,7 @@ mkdir -p $TMPDIR
 
 if [ ! -f ${FULLNAME}_MACS2.done ]; then 
 	 macs2 callpeak -t $FULLNAME -g 1.2e8 -n ${filename}_MACS2_noInput --outdir ${path} --nomodel --shift -100 --extsize 200 -B -q 0.01
-         macs2 callpeak -t $full -g 1.2e8 -n ${filename}_MACS2_paired  --outdir ${path}  -B -q 0.01 -f BAMPE  
+         macs2 callpeak -t $FULLNAME -g 1.2e8 -n ${filename}_MACS2_paired  --outdir ${path}  -B -q 0.01 -f BAMPE  
 
 	 touch ${FULLNAME}_MACS2.done
 fi
