1db1ff94fa08325d143d39bb272ec5068185b05c
Axelsson <elin.axelsson@login1.ice.gmi.oeaw.ac.at>
2017-06-26 13:15:27 +0000

    tpm of narrowpeaks

---
+++ bash/calculate_tpm_in_narrowpeak_main.sh
@@ -0,0 +1,39 @@
+
+
+
+PARAM_IDX=$PBS_ARRAY_INDEX
+PARAM_FILE=$PBS_O_WORKDIR/files.tab
+settingsfile=$PBS_O_WORKDIR/setting.txt
+outname=$(grep outname $settingsfile | awk '{print $2}')
+type=$(sed "${PARAM_IDX}q;d" $PARAM_FILE | awk '{print $4}')
+
+NAME=$(sed "${PARAM_IDX}q;d" $PARAM_FILE | awk '{print $1}')
+ALIGN=$(sed "${PARAM_IDX}q;d" $PARAM_FILE | awk '{print $2}')
+workpath=$(grep workpath $settingsfile | awk '{print $2}')
+PATH=${workpath}/${outname}/${type}
+
+filetype="uniq_filtered"
+peaktype=( "MACS2_paired" "MACS2_noInput" )
+
+for j in "${peaktype[@]}"
+do
+    gff=${PATH}/${type}.${filetype}.bam_${j}_peaks.narrowPeak.gff
+echo $gff
+    counts=${PATH}/$NAME.${filetype}_${j}_counts.tab 
+echo $counts
+    out=${PATH}/$NAME.${j}_length.tab
+echo $out
+    joined=${PATH}/$NAME.${j}_joined.tab
+echo $joined
+    tpm=${PATH}/$NAME.${j}_tpm.tab
+echo $tmp
+
+    awk '{ $11 = $5 - $4 } { print $10 "\t" $11} ' $gff | tr -d '""' | tr -d ";" > $out
+    join -j1 <(grep -v __ $counts | sort -t$'\t' -n --key=1.5 ) <(sort -t$'\t' -n --key=1.5 $out) > $joined
+    awk '{$4=$2/($3/1000)} 1' $joined > $joined.tmp
+    mv $joined.tmp $joined
+    total=$(awk 'BEGIN{ total=0 } { total=total+$4 } END{ printf total }' $joined)
+	echo $total
+    awk -v total=$total '{ print $1 "\t" 1000000*$4/total }' $joined > $tpm
+
+done
