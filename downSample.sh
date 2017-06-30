5d079a73732b9d7b566517e2f4b1cd7b5e2db9e6
Axelsson <elin.axelsson@login1.ice.gmi.oeaw.ac.at>
2017-06-30 11:21:18 +0000

    ready to run!!??

--- downSample.sh
+++ downSample.sh
@@ -3,8 +3,8 @@
 #PBS -N subsamp
 #PBS -P berger_common
 #PBS -q workq
-#PBS -l walltime=01:00:00
-#PBS -J 1-7
+#PBS -l walltime=10:00:00
+#PBS -J 1-4
 
 
 # === begin ENVIRONMENT SETUP ===
@@ -15,6 +15,7 @@ module load SAMtools/1.3-goolf-1.4.10
 PARAM_IDX=$PBS_ARRAY_INDEX
 PARAM_FILE=$PBS_O_WORKDIR/files2.tab
 settingsfile=$PBS_O_WORKDIR/setting.txt
+prop=$(sed "${PARAM_IDX}q;d" $PARAM_FILE | awk '{print $7}')
 
 NAME=$(sed "${PARAM_IDX}q;d" $PARAM_FILE | awk '{print $1}')
 workpath=$(grep workpath $settingsfile | awk '{print $2}')
@@ -22,12 +23,16 @@ bampath=$(grep bampath $settingsfile | awk '{print $2}')
 index=$(grep index $settingsfile | awk '{print $2}')
 alignpath=$(grep alignpath $settingsfile | awk '{print $2}')
 
+echo $prop
+echo $NAME
 # === START ===
 
 myout=$PBS_O_WORKDIR/bam
 
 input=${myout}/${NAME}.aligned_sorted_cut1.bam
 
-output=${myout}/${NAME}.aligned_sorted_cut1_small.bam
+output=${myout}/${NAME}.aligned_sorted_cut1_sub.bam
 
-samtools view -s 0.01 -b $input > $output
+#if [ "${prop}" -ne 1 ];then 
+samtools view -s ${prop} -b $input > $output
+#fi
