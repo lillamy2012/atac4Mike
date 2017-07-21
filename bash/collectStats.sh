79bc3f79237a6c52c44ed83a545f8a40acb71d73
Axelsson <elin.axelsson@login1.ice.gmi.oeaw.ac.at>
2017-07-21 12:40:47 +0000

    Removed old files no longer needed

--- bash/collectStats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file=$1
-
-for stat in "raw total sequences:" "reads mapped:" "reads mapped and paired:" "reads properly paired:" "reads duplicated:" "average length:" "maximum length:" "average quality:" "insert size average:" "insert size standard deviation:" "inward oriented pairs:" "outward oriented pairs:" "pairs with other orientation:" "pairs on different chromosomes:" ; do
-grep "^SN" $file | grep "$stat" | awk -F $'\t' '{ print $2,"\t",$3}'
-done
-
-
